84c22dd4806ad8c44b61de7d84cea9c060eb28ea
Lukáš Novotný <lukas@algorim.com>
2015-11-30 12:55:57 +0100

    fix travis

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 dnvm use default
 dnu restore
-dnu build ./src/* ./samples/* --framework dotnet5.4 --configuration Release
+dnu build ./src/* --framework dotnet5.4 --configuration Release
+dnu build ./samples/* --framework dnxcore50 --configuration Release
