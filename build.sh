8c7727a284bda5b14833c2814189c81f9dcfe2d5
Lukáš Novotný <lukas@algorim.com>
2016-04-26 20:26:41 +0200

    fix build warnings; travis update

--- build.sh
+++ build.sh
@@ -1,6 +1,60 @@
 #!/bin/bash
 
-dnvm use default
-dnu restore
-dnu build ./src/* --framework dotnet5.6 --configuration Release
-dnu build ./samples/* --framework dnxcore50 --configuration Release
+function _build
+{
+    pushd $1
+    dotnet build --configuration Release
+    popd
+}
+
+function _pack
+    pushd $1
+    dotnet pack --output ../../build --configuration Release --version-suffix $buildNumber
+    popd
+}
+
+function _test {
+    pushd $1
+    dotnet test --configuration Release
+    popd
+}
+
+echo
+echo "Display dotnet info.."
+echo
+dotnet --info
+
+echo
+echo "Restore packages.."
+echo
+Exec { dotnet restore }
+
+echo
+echo "Build & pack libraries.."
+echo
+
+BUILD_NUMBER = $env:APPVEYOR_BUILD_NUMBER
+if [ -z "$BUILD_NUMBER" ]; then
+    BUILD_NUMBER=local
+fi
+
+_pack ".\src\Odachi.AspNetCore.Authentication.Basic"
+_pack ".\src\Odachi.AspNetCore.Mvc"
+_pack ".\src\Odachi.AspNetCore.MvcPages"
+_pack ".\src\Odachi.Data"
+_pack ".\src\Odachi.Gettext"
+_pack ".\src\Odachi.Localization"
+_pack ".\src\Odachi.Localization.Extraction"
+_pack ".\src\Odachi.Localization.Extraction.Commands"
+_pack ".\src\Odachi.Security"
+
+echo
+echo "Build samples.."
+echo
+_build ".\samples\BasicAuthenticationSample"
+
+echo
+echo "Build & run test.."
+echo
+_test ".\test\Odachi.Gettext.Tests"
+_test ".\test\Odachi.Localization.Extraction.Tests"
