83283cc217a2caf2cd5af31a0e19456b1b01997f
Lukáš Novotný <lukas@algorim.com>
2016-05-14 12:16:24 +0200

    swap to preview1 tooling

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # source: https://github.com/enricosada/fsharp-dotnet-cli-samples
 
 # Download script to install dotnet cli
-curl -L --create-dirs https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/install.sh -o ./tools/install.sh
+curl -L --create-dirs https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0-preview1/scripts/obtain/install.sh -o ./tools/install.sh
 find ./tools -name "*.sh" -exec chmod +x {} \;
 
 export DOTNET_INSTALL_DIR="$PWD/.dotnetcli"
