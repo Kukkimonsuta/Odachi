344c5ed01f3e40c1650828edb898fa5f699263a9
Lukáš Novotný <lukas@algorim.com>
2016-05-16 19:01:04 +0200

    add security tests to ci

--- build.sh
+++ build.sh
@@ -46,3 +46,4 @@ echo "Build & run test.."
 echo
 _test "./test/Odachi.Gettext.Tests" netcoreapp10
 _test "./test/Odachi.Localization.Extraction.Tests" netcoreapp10
+_test "./test/Odachi.Security.Tests" netcoreapp13
