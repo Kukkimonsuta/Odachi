0d63500c4b2816bb86a37e01cdb6da0ff24a3744
Lukáš Novotný <lukas@algorim.com>
2016-05-16 18:40:15 +0200

    added parameter support to permissions

--- src/Odachi.Security/PrincipalExtensions.cs
+++ src/Odachi.Security/PrincipalExtensions.cs
@@ -14,28 +14,208 @@ namespace Odachi.Security
         /// <returns>True when permission is granted, false otherwise</returns>
         public static bool HasPermission(this ClaimsPrincipal principal, Permission permission)
         {
-            return principal.HasClaim(Permission.PermissionClaim, permission.Name);
-        }
+			foreach (var claim in principal.Claims)
+			{
+				if (claim.Type != Permission.PermissionClaim)
+					continue;
 
-        /// <summary>
-        /// Throws exception when principal has not been granted a permission.
-        /// </summary>
-        /// <param name="principal">The principal</param>
-        /// <param name="permission">The permission</param>
-        public static void DemandPermission(this ClaimsPrincipal principal, Permission permission)
+				if (permission.Matches(claim.Value))
+					return true;
+			}
+
+			return false;
+        }
+		/// <summary>
+		/// Returns whether principal has been granted a permission.
+		/// </summary>
+		/// <param name="principal">The principal</param>
+		/// <param name="permission">The permission</param>
+		/// <param name="arg0">The first permission argument</param>
+		/// <returns>True when permission is granted, false otherwise</returns>
+		public static bool HasPermission(this ClaimsPrincipal principal, Permission permission, object arg0)
+		{
+			foreach (var claim in principal.Claims)
+			{
+				if (claim.Type != Permission.PermissionClaim)
+					continue;
+
+				if (permission.Matches(claim.Value, arg0))
+					return true;
+			}
+
+			return false;
+		}
+		/// <summary>
+		/// Returns whether principal has been granted a permission.
+		/// </summary>
+		/// <param name="principal">The principal</param>
+		/// <param name="permission">The permission</param>
+		/// <param name="arg0">The first permission argument</param>
+		/// <param name="arg1">The second permission argument</param>
+		/// <returns>True when permission is granted, false otherwise</returns>
+		public static bool HasPermission(this ClaimsPrincipal principal, Permission permission, object arg0, object arg1)
+		{
+			foreach (var claim in principal.Claims)
+			{
+				if (claim.Type != Permission.PermissionClaim)
+					continue;
+
+				if (permission.Matches(claim.Value, arg0, arg1))
+					return true;
+			}
+
+			return false;
+		}
+		/// <summary>
+		/// Returns whether principal has been granted a permission.
+		/// </summary>
+		/// <param name="principal">The principal</param>
+		/// <param name="permission">The permission</param>
+		/// <param name="args">Permission arguments</param>
+		/// <returns>True when permission is granted, false otherwise</returns>
+		public static bool HasPermission(this ClaimsPrincipal principal, Permission permission, params object[] args)
+		{
+			foreach (var claim in principal.Claims)
+			{
+				if (claim.Type != Permission.PermissionClaim)
+					continue;
+
+				if (permission.Matches(claim.Value, args))
+					return true;
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		/// Throws exception when principal has not been granted a permission.
+		/// </summary>
+		/// <param name="principal">The principal</param>
+		/// <param name="permission">The permission</param>
+		public static void DemandPermission(this ClaimsPrincipal principal, Permission permission)
         {
             if (!HasPermission(principal, permission))
                 throw new SecurityException("Principal '" + principal.Identity.Name + "' doesn't have permission '" + permission.Name + "'");
         }
+		/// <summary>
+		/// Throws exception when principal has not been granted a permission.
+		/// </summary>
+		/// <param name="principal">The principal</param>
+		/// <param name="permission">The permission</param>
+		/// <param name="arg0">The first permission argument</param>
+		public static void DemandPermission(this ClaimsPrincipal principal, Permission permission, object arg0)
+		{
+			if (!HasPermission(principal, permission, arg0))
+				throw new SecurityException("Principal '" + principal.Identity.Name + "' doesn't have permission '" + permission.Name + "'+1");
+		}
+		/// <summary>
+		/// Throws exception when principal has not been granted a permission.
+		/// </summary>
+		/// <param name="principal">The principal</param>
+		/// <param name="permission">The permission</param>
+		/// <param name="arg0">The first permission argument</param>
+		/// <param name="arg1">The second permission argument</param>
+		public static void DemandPermission(this ClaimsPrincipal principal, Permission permission, object arg0, object arg1)
+		{
+			if (!HasPermission(principal, permission, arg0, arg1))
+				throw new SecurityException("Principal '" + principal.Identity.Name + "' doesn't have permission '" + permission.Name + "'+2");
+		}
+		/// <summary>
+		/// Throws exception when principal has not been granted a permission.
+		/// </summary>
+		/// <param name="principal">The principal</param>
+		/// <param name="permission">The permission</param>
+		/// <param name="args">Permission arguments</param>
+		public static void DemandPermission(this ClaimsPrincipal principal, Permission permission, params object[] args)
+		{
+			if (!HasPermission(principal, permission, args))
+				throw new SecurityException("Principal '" + principal.Identity.Name + "' doesn't have permission '" + permission.Name + "'+" + args.Length);
+		}
 
+		/// <summary>
+		/// Returns whether principal has been granted a permission (assumes principal is ClaimsPrincipal).
+		/// </summary>
+		/// <param name="principal">The principal</param>
+		/// <param name="permission">The permission</param>
+		/// <returns>True when permission is granted, false otherwise</returns>
 		public static bool HasPermission(this IPrincipal principal, Permission permission)
 		{
 			return ((ClaimsPrincipal)principal).HasPermission(permission);
 		}
+		/// <summary>
+		/// Returns whether principal has been granted a permission (assumes principal is ClaimsPrincipal).
+		/// </summary>
+		/// <param name="principal">The principal</param>
+		/// <param name="permission">The permission</param>
+		/// <param name="arg0">The first permission argument</param>
+		/// <returns>True when permission is granted, false otherwise</returns>
+		public static bool HasPermission(this IPrincipal principal, Permission permission, object arg0)
+		{
+			return ((ClaimsPrincipal)principal).HasPermission(permission, arg0);
+		}
+		/// <summary>
+		/// Returns whether principal has been granted a permission (assumes principal is ClaimsPrincipal).
+		/// </summary>
+		/// <param name="principal">The principal</param>
+		/// <param name="permission">The permission</param>
+		/// <param name="arg0">The first permission argument</param>
+		/// <param name="arg1">The second permission argument</param>
+		/// <returns>True when permission is granted, false otherwise</returns>
+		public static bool HasPermission(this IPrincipal principal, Permission permission, object arg0, object arg1)
+		{
+			return ((ClaimsPrincipal)principal).HasPermission(permission, arg0, arg1);
+		}
+		/// <summary>
+		/// Returns whether principal has been granted a permission (assumes principal is ClaimsPrincipal).
+		/// </summary>
+		/// <param name="principal">The principal</param>
+		/// <param name="permission">The permission</param>
+		/// <param name="args">Permission arguments</param>
+		/// <returns>True when permission is granted, false otherwise</returns>
+		public static bool HasPermission(this IPrincipal principal, Permission permission, params object[] args)
+		{
+			return ((ClaimsPrincipal)principal).HasPermission(permission, args);
+		}
 
+		/// <summary>
+		/// Throws exception when principal has not been granted a permission (assumes principal is ClaimsPrincipal).
+		/// </summary>
+		/// <param name="principal">The principal</param>
+		/// <param name="permission">The permission</param>
 		public static void DemandPermission(this IPrincipal principal, Permission permission)
 		{
 			((ClaimsPrincipal)principal).DemandPermission(permission);
+		}
+		/// <summary>
+		/// Throws exception when principal has not been granted a permission (assumes principal is ClaimsPrincipal).
+		/// </summary>
+		/// <param name="principal">The principal</param>
+		/// <param name="permission">The permission</param>
+		/// <param name="arg0">The first permission argument</param>
+		public static void DemandPermission(this IPrincipal principal, Permission permission, object arg0)
+		{
+			((ClaimsPrincipal)principal).DemandPermission(permission, arg0);
+		}
+		/// <summary>
+		/// Throws exception when principal has not been granted a permission (assumes principal is ClaimsPrincipal).
+		/// </summary>
+		/// <param name="principal">The principal</param>
+		/// <param name="permission">The permission</param>
+		/// <param name="arg0">The first permission argument</param>
+		/// <param name="arg1">The second permission argument</param>
+		public static void DemandPermission(this IPrincipal principal, Permission permission, object arg0, object arg1)
+		{
+			((ClaimsPrincipal)principal).DemandPermission(permission, arg0, arg1);
+		}
+		/// <summary>
+		/// Throws exception when principal has not been granted a permission (assumes principal is ClaimsPrincipal).
+		/// </summary>
+		/// <param name="principal">The principal</param>
+		/// <param name="permission">The permission</param>
+		/// <param name="args">Permission arguments</param>
+		public static void DemandPermission(this IPrincipal principal, Permission permission, params object[] args)
+		{
+			((ClaimsPrincipal)principal).DemandPermission(permission, args);
 		}
 	}
 }
