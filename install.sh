e3cd41140ec2228bb3793b6118f97651d7496d51
Lukáš Novotný <lukas@algorim.com>
2016-05-18 01:47:15 +0200

    another attempt at travis fix

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 # source: https://github.com/enricosada/fsharp-dotnet-cli-samples
 
 # Download script to install dotnet cli
-curl -L --create-dirs "https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0-preview1/scripts/obtain/install.sh" -o ./tools/install.sh
+curl -L --create-dirs "https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.sh" -o ./tools/dotnet-install.sh
 find ./tools -name "*.sh" -exec chmod +x {} \;
 
 export DOTNET_INSTALL_DIR="$PWD/.dotnetcli"
 # use bash to workaround bug https://github.com/dotnet/cli/issues/1725
-sudo bash ./tools/install.sh --install-dir "$DOTNET_INSTALL_DIR" --no-path --verbose
+sudo bash ./tools/dotnet-install.sh --version 1.0.0-preview1-002702 --install-dir "$DOTNET_INSTALL_DIR" --no-path --verbose
 # add dotnet to PATH
 export PATH="$DOTNET_INSTALL_DIR:$PATH"
