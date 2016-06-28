031771de943021dc4840e51b9a7fe3e873e2cb69
Lukáš Novotný <lukas@algorim.com>
2016-06-28 18:57:55 +0200

    ci: use preview2 tooling

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 # source: https://github.com/enricosada/fsharp-dotnet-cli-samples
 
 # Download script to install dotnet cli
-curl -L --create-dirs "https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.sh" -o ./tools/dotnet-install.sh
+curl -L --create-dirs "https://raw.githubusercontent.com/dotnet/cli/v1.0.0-preview2/scripts/obtain/dotnet-install.sh" -o ./tools/dotnet-install.sh
 find ./tools -name "*.sh" -exec chmod +x {} \;
 
 export DOTNET_INSTALL_DIR="$PWD/.dotnetcli"
 # use bash to workaround bug https://github.com/dotnet/cli/issues/1725
-sudo bash ./tools/dotnet-install.sh --install-dir "$DOTNET_INSTALL_DIR" --no-path --verbose
+sudo bash ./tools/dotnet-install.sh --channel preview --version 1.0.0-preview2-003121 --install-dir "$DOTNET_INSTALL_DIR" --no-path --verbose
 # add dotnet to PATH
 export PATH="$DOTNET_INSTALL_DIR:$PATH"
