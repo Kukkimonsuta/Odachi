dac2519d5e3101afd874349ab2f40b55a9549bc1
Lukáš Novotný <lukas@dramiel.com>
2017-06-02 22:15:32 +0200

    add constructors for value or state

--- src/Odachi.Validation/ValueOrState.cs
+++ src/Odachi.Validation/ValueOrState.cs
@@ -5,17 +5,36 @@
 	/// </summary>
 	public struct ValueOrState<TValue>
 	{
+		public ValueOrState(TValue value)
+		{
+			Value = value;
+			State = null;
+		}
+		public ValueOrState(ValidationState state)
+		{
+			Value = default(TValue);
+			State = state;
+		}
+
 		public TValue Value { get; private set; }
 
 		public ValidationState State { get; private set; }
 
 		#region Static members
-
+		
 		public static implicit operator ValueOrState<TValue>(Validator validator)
 		{
 			return new ValueOrState<TValue>()
 			{
 				State = validator.State,
+			};
+		}
+
+		public static implicit operator ValueOrState<TValue>(ValidationState state)
+		{
+			return new ValueOrState<TValue>()
+			{
+				State = state,
 			};
 		}
 
