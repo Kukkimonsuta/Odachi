02e0e2474a12f9cc18d828cba8b837f090e81787
Unknown <lukas@algorim.com>
2017-06-09 00:32:07 +0200

    jsonrpc: remove self description as it complicates codegen in current state

--- src/Odachi.AspNetCore.JsonRpc/Modules/ServerModule.cs
+++ src/Odachi.AspNetCore.JsonRpc/Modules/ServerModule.cs
@@ -24,31 +24,5 @@ namespace Odachi.AspNetCore.JsonRpc.Modules
 		{
 			return DateTime.Now;
 		}
-
-		[RpcMethod]
-		public static Task<object[]> DescribeAsync(JsonRpcServer server, bool includeInternals = false)
-		{
-			var modules = server.Methods
-				.GroupBy(m => m.ModuleName, (k, g) => new
-				{
-					Name = k,
-					Methods = g.Select(m => new
-					{
-						Name = m.MethodName,
-						ReturnType = m.ReturnType?.JsonType.ToString().ToLowerInvariant(),
-						Parameters = m.Parameters.Where(p => includeInternals || !p.IsInternal).Select(p => new
-						{
-							Name = p.Name,
-							Type = p.Type.JsonType.ToString().ToLowerInvariant(),
-							IsOptional = p.IsOptional,
-							DefaultValue = p.DefaultValue,
-						}),
-					}),
-				})
-				.Cast<object>()
-				.ToArray();
-
-			return Task.FromResult(modules);
-		}
 	}
 }
