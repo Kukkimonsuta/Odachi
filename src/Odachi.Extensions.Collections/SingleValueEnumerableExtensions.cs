fb6d5fa438498ee9de9e473d8e20e61139ab5cd3
Unknown <lukas@algorim.com>
2017-06-23 13:07:09 +0200

    collections: add `SingleValueEnumerable`

---
+++ src/Odachi.Extensions.Collections/SingleValueEnumerableExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odachi.Extensions.Collections
+{
+    public static class SingleValueEnumerableExtensions
+    {
+		/// <summary>
+		/// Wraps <paramref name="value"/> in <see cref="SingleValueEnumerable{T}"/>.
+		/// </summary>
+		/// <typeparam name="T">Value type</typeparam>
+		/// <param name="value">Value</param>
+		/// <returns>Value as <see cref="SingleValueEnumerable{T}"/></returns>
+		public static SingleValueEnumerable<T> AsSingleValueEnumerable<T>(this T value)
+		{
+			return new SingleValueEnumerable<T>(value);
+		}
+	}
+}
