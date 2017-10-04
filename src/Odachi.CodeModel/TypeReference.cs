50ea43a917cd2b2a8385138fc24bc443eb53073f
Unknown <lukas@algorim.com>
2017-10-04 16:53:25 +0200

    codemodel: add type kind

--- src/Odachi.CodeModel/TypeReference.cs
+++ src/Odachi.CodeModel/TypeReference.cs
@@ -1,15 +1,49 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Odachi.CodeModel.Mapping;
+using System.Reflection;
 
 namespace Odachi.CodeModel
 {
+	public enum TypeKind
+	{
+		Primitive,
+		Interface,
+		Enum,
+		Struct,
+		Class,
+	}
+
+	public static class TypeKindExtensions
+	{
+		public static TypeKind GetTypeKind(this Type type)
+		{
+			return type.GetTypeInfo().GetTypeKind();
+		}
+		public static TypeKind GetTypeKind(this TypeInfo info)
+		{
+			if (info.IsPrimitive)
+				return TypeKind.Primitive;
+
+			if (info.IsInterface)
+				return TypeKind.Interface;
+
+			if (info.IsEnum)
+				return TypeKind.Enum;
+
+			if (info.IsValueType)
+				return TypeKind.Struct;
+
+			return TypeKind.Class;
+		}
+	}
+
 	public class TypeReference
 	{
-		public TypeReference(string module, string name, bool isNullable, params TypeReference[] genericArguments)
+		public TypeReference(string module, string name, TypeKind kind, bool isNullable, params TypeReference[] genericArguments)
 		{
 			if (name == null)
 				throw new ArgumentNullException(nameof(name));
@@ -18,15 +52,17 @@ namespace Odachi.CodeModel
 
 			Module = module;
 			Name = name;
+			Kind = kind;
 			IsNullable = isNullable;
 			GenericArguments = genericArguments;
 		}
-		
+
 		public string Module { get; }
 		public string Name { get; }
+		public TypeKind Kind { get; }
 		public bool IsNullable { get; set; }
 		public TypeReference[] GenericArguments { get; }
-		
+
 		public override string ToString()
 		{
 			return $"{Name}{(GenericArguments.Length > 0 ? $"<{string.Join(", ", GenericArguments.Select(a => a.ToString()))}>" : "")}{(IsNullable ? "?" : "")}";
