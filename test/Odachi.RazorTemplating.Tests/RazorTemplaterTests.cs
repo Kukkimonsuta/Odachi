aee172b797ca93055c2ccdbb7602bcebe95f47e0
Lukáš Novotný <lukas@algorim.com>
2019-10-10 20:12:04 +0200

    comment out bogus test

--- test/Odachi.RazorTemplating.Tests/RazorTemplaterTests.cs
+++ test/Odachi.RazorTemplating.Tests/RazorTemplaterTests.cs
@@ -10,38 +10,38 @@ namespace Odachi.RazorTemplating.Tests
 {
 	public class RazorTemplaterTests
 	{
-		[Fact]
-		public void Works()
-		{
-			var OutputDirectory = "d:\\razor-temp";
+		//[Fact]
+		//public void Works()
+		//{
+		//	var OutputDirectory = "d:\\razor-temp";
 
-			// normalize project directory (must include trailing slash)
-			var ProjectDirectory = Directory.GetCurrentDirectory();
-			if (ProjectDirectory[ProjectDirectory.Length - 1] != Path.DirectorySeparatorChar)
-				ProjectDirectory += Path.DirectorySeparatorChar;
+		//	// normalize project directory (must include trailing slash)
+		//	var ProjectDirectory = Directory.GetCurrentDirectory();
+		//	if (ProjectDirectory[ProjectDirectory.Length - 1] != Path.DirectorySeparatorChar)
+		//		ProjectDirectory += Path.DirectorySeparatorChar;
 
-			// create templater
-			var templater = new RazorTemplater(ProjectDirectory);
+		//	// create templater
+		//	var templater = new RazorTemplater(ProjectDirectory);
 
-			// create templater
-			var outputFileNames = new List<string>();
-			foreach (var inputItem in Directory.GetFiles(ProjectDirectory, "*.cshtml", SearchOption.AllDirectories))
-			{
-				var inputFileName = inputItem;
-				var outputFileName = Path.Combine(
-					OutputDirectory,
-					PathTools.GetRelativePath(ProjectDirectory, Path.GetDirectoryName(inputFileName)).Replace(Path.DirectorySeparatorChar, '_').Replace(Path.AltDirectorySeparatorChar, '_') + "_" + Path.GetFileNameWithoutExtension(inputFileName) + ".cs"
-				);
+		//	// create templater
+		//	var outputFileNames = new List<string>();
+		//	foreach (var inputItem in Directory.GetFiles(ProjectDirectory, "*.cshtml", SearchOption.AllDirectories))
+		//	{
+		//		var inputFileName = inputItem;
+		//		var outputFileName = Path.Combine(
+		//			OutputDirectory,
+		//			PathTools.GetRelativePath(ProjectDirectory, Path.GetDirectoryName(inputFileName)).Replace(Path.DirectorySeparatorChar, '_').Replace(Path.AltDirectorySeparatorChar, '_') + "_" + Path.GetFileNameWithoutExtension(inputFileName) + ".cs"
+		//		);
 
-				if (!Directory.Exists(OutputDirectory))
-				{
-					Directory.CreateDirectory(OutputDirectory);
-				}
+		//		if (!Directory.Exists(OutputDirectory))
+		//		{
+		//			Directory.CreateDirectory(OutputDirectory);
+		//		}
 
-				templater.Generate(inputFileName, outputFileName);
+		//		templater.Generate(inputFileName, outputFileName);
 
-				outputFileNames.Add(outputFileName);
-			}
-		}
+		//		outputFileNames.Add(outputFileName);
+		//	}
+		//}
 	}
 }
