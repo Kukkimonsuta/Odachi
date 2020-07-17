0113723deee43595b99c90f947fe8170e161f4c4
Lukáš Novotný <lukas@dramiel.com>
2020-07-17 23:12:58 +0200

    codemodel.jsonrpc: try to infer return nullability

--- src/Odachi.CodeModel.Providers.JsonRpc/PackageExtensions.cs
+++ src/Odachi.CodeModel.Providers.JsonRpc/PackageExtensions.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Odachi.CodeModel.Builders;
 using Odachi.CodeModel.Mapping;
 using Odachi.CodeModel.Providers.JsonRpc.Description;
 using Odachi.Extensions.Formatting;
+using Odachi.Extensions.Reflection;
+using Odachi.JsonRpc.Server.Internal;
 using Odachi.JsonRpc.Server.Model;
 
 namespace Odachi.CodeModel
@@ -48,8 +51,27 @@ namespace Odachi.CodeModel
 				{
 					foreach (var method in methods)
 					{
+						var isNullable = !method.ReturnType.IsNonNullableValueType();
+
+						// TODO: jsonrpc server should probably have only reflected methods
+						if (method is ReflectedJsonRpcMethod reflectedJsonRpcMethod)
+						{
+							var returnType = reflectedJsonRpcMethod.Method.ReturnType;
+
+							if (returnType.IsGenericType && (returnType.GetGenericTypeDefinition() == typeof(Task<>) || returnType.GetGenericTypeDefinition() == typeof(ValueTask<>)))
+							{
+								isNullable = !reflectedJsonRpcMethod.Method.ReturnParameter.IsGenericArgumentNonNullable(0);
+							}
+							else
+							{
+								isNullable = !reflectedJsonRpcMethod.Method.ReturnParameter.IsNonNullable();
+							}
+						}
+
 						service.Method(method.MethodName.ToPascalInvariant(), method.ReturnType ?? typeof(void), method, m =>
 						{
+							m.ReturnType.IsNullable = isNullable;
+
 							foreach (var parameter in method.Parameters.Where(p => p.Source == JsonRpcParameterSource.Request))
 							{
 								m.Parameter(parameter.Name, parameter.Type, parameter);
